bcd8d00ab107732c4dae027007e0a8eda8121bbe
hbc <bcxxxxxx@gmail.com>
2013-09-17 14:40:00 +0800

    init

---
+++ src/com/gdut/library/makemake/MakemakeActivity.java
@@ -0,0 +1,15 @@
+package com.gdut.library.makemake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MakemakeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
