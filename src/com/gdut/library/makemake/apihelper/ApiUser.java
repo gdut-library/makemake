0bd53b92f76aa9da553385cf6c7cc48195507d2f
hbc <bcxxxxxx@gmail.com>
2013-09-29 15:21:31 +0800

    added api user constructor

--- src/com/gdut/library/makemake/apihelper/ApiUser.java
+++ src/com/gdut/library/makemake/apihelper/ApiUser.java
@@ -1,4 +1,25 @@
 package com.gdut.library.makemake.apihelper;
 
+import org.json.JSONObject;
+import org.json.JSONException;
 
-public class ApiUser {}
+public class ApiUser {
+    public String name;
+    public String cardno;
+    public String faculty;
+    public String major;
+
+    ApiUser() {}
+
+    ApiUser(JSONObject infos)
+        throws JSONException {
+        try {
+            name = infos.getString("name");
+            cardno = infos.getString("cardno");
+            faculty = infos.getString("faculty");
+            major = infos.getString("major");
+        } catch (JSONException e) {
+            throw e;
+        }
+    }
+}
